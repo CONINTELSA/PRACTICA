1babc61a13efea3f8169c5a3b1f8a327729f066b
EDGAR <ventas.esm@hotmail.com>
2022-09-20 23:09:01 +0000

    nuevo trabajos de clases

--- suma.cpp
+++ suma.cpp
@@ -1,1 +1,17 @@
-hola mundo sor edgar
+//Creado por stalin
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float x,y,z;
+ cout<<"Ingrese 2 numero x y :";
+ cin>>x>>y;
+ z=x+y;
+ cout<<"El resutado es : ";
+ cout<<z<<endl;
+
+ return 0;
+
+}
